6042666a32899ad61d8db655565d2981d4833caa
Viktor at Linemark <viktor@linemark.studio>
2026-06-17 19:23:21 +0200

    add nav-links

--- app/components/ui/navigation/navigation-bar.tsx
+++ app/components/ui/navigation/navigation-bar.tsx
@@ -12,6 +12,35 @@ export default function NavigationBar() {
                                 <Logo size="md" />
                             </Link>
                         </div>
+                        <div className="group/links flex items-center justify-center gap-x-2 px-2 w-auto">
+                            <Link href="/work" className="h-full w-full cursor-pointer group-hover/links:opacity-50 hover:group-hover/links:opacity-100 transition-all duration-150 ease-linear">
+                                <span className="flex items-center justify-center gap-x-2 px-2 py-2 w-auto">
+                                    <span className="pl-2 block">
+                                        <span className="font-mono text-base leading-4 tracking-normal uppercase relative" >
+                                            Work
+                                        </span>
+                                    </span>
+                                </span>
+                            </Link>
+                            <Link href="/about" className="h-full w-full cursor-pointer group-hover/links:opacity-50 hover:group-hover/links:opacity-100 transition-all duration-150 ease-linear">
+                                <span className="flex items-center justify-center gap-x-2 px-2 py-2 w-auto">
+                                    <span className="pl-2 block">
+                                        <span className="font-mono text-base leading-4 tracking-normal uppercase relative" >
+                                            About
+                                        </span>
+                                    </span>
+                                </span>
+                            </Link>
+                            <Link href="/contact" className="h-full w-full cursor-pointer group-hover/links:opacity-50 hover:group-hover/links:opacity-100 transition-all duration-150 ease-linear">
+                                <span className="flex items-center justify-center gap-x-2 px-2 py-2 w-auto">
+                                    <span className="pl-2 block">
+                                        <span className="font-mono text-base leading-4 tracking-normal uppercase relative" >
+                                            Contact
+                                        </span>
+                                    </span>
+                                </span>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
